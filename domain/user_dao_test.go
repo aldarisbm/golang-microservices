7af2cc039c4a54b37fe82549e361d275800f0f58
Jose Berrio <aldarisbm@gmail.com>
2020-11-15 12:54:41 -0600

    added more tests

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -3,18 +3,16 @@ package domain
 import (
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserNoUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
-	if user != nil {
-		t.Error("A user was not expected with ID 0")
-	}
-	if err == nil {
-		t.Error("An error was expect for user ID 0")
-	}
-	if err.StatusCode != http.StatusNotFound {
-		t.Error("A StatusNotFound 404 status was expected")
-	}
+	assert.Nil(t, user, "user was not expected")
+	assert.NotNil(t, err, "were expecting an error but didnt get one")
+	assert.EqualValues(t, err.StatusCode, http.StatusNotFound)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 does not exist", err.Message)
 }
