5b535a98bff0ef91cfd0cc9253e7e67d0d682482
Jose Berrio <aldarisbm@gmail.com>
2020-11-16 08:29:39 -0600

    added GetUserFound test

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -16,3 +16,15 @@ func TestGetUserNoUserFound(t *testing.T) {
 	assert.EqualValues(t, "not_found", err.Code)
 	assert.EqualValues(t, "user 0 does not exist", err.Message)
 }
+
+func TestGetUserUserFound(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err, "error was not expected")
+	assert.NotNil(t, user, "User was expected, and not found")
+
+	assert.EqualValues(t, 123, user.ID)
+	assert.EqualValues(t, "Hello", user.FirstName)
+	assert.EqualValues(t, "World", user.LastName)
+	assert.EqualValues(t, "hello@world.com", user.Email)
+}
